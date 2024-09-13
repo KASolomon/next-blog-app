06cc8c3143a70af69bb2c8e99e80ad2a2c349686
KASolomon <kusiappiahsolomon10@gmail.com>
2024-09-13 14:56:40 +0000

    design contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <div className="flex ">
+      <div className=" flex-1 ">
+        <div className="relative w-3/4 h-3/4 m-auto">
+          <Image src={"/contact.png"} alt="contactImg" fill />
+        </div>
+      </div>
+      <div className=" flex-1">
+        <form action="" className="flex flex-col gap-10">
+          <input
+            className="p-6 bg-bgSoft"
+            type="text"
+            name=""
+            id=""
+            placeholder="Full name"
+          />
+          <input
+            className="p-6 bg-bgSoft"
+            type="email"
+            name=""
+            id=""
+            placeholder="Email (optional)"
+          />
+          <input
+            className="p-6 bg-bgSoft"
+            type="number"
+            name=""
+            id=""
+            placeholder="Phone number (optional)"
+          />
+          <textarea
+            className="p-6 bg-bgSoft"
+            name=""
+            id=""
+            placeholder="Your comments..."
+            cols={30}
+            rows={10}
+          ></textarea>
+          <button className="bg-btn w-full p-6 rounded-md">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
