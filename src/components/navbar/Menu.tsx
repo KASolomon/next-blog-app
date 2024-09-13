32c1eb2a6cbf2ba27241460c9b34867b0cc36867
KASolomon <kusiappiahsolomon10@gmail.com>
2024-09-13 15:39:24 +0000

    improve menu component

--- src/components/navbar/Menu.tsx
+++ src/components/navbar/Menu.tsx
@@ -1,14 +1,23 @@
 "use client";
-import React, { useState } from "react";
-import { NavItem } from "./NavLink";
+import { useState } from "react";
 import Links from "./Links";
+import Image from "next/image";
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <button onClick={() => setIsOpen(!isOpen)}>Menu</button>
-      <div className={`${isOpen && "hidden"} absolute flex flex-col min-h-screen items-center top-20 right-0 justify-center w-auto p-8 bg-background`}>
+      <div style={{display :'contents'}}>
+      
+      <button onClick={() => setIsOpen(!isOpen)}>
+        <Image src="/menu.png" height={28} width={30} className="top-2" alt="" />
+      </button>
+      </div>
+      <div
+        className={`${
+          !isOpen && "hidden"
+        } absolute z-50 flex flex-col min-h-screen items-center top-20 right-0  gap-4 justify-center w-auto p-8 bg-background`}
+      >
         <Links />
       </div>
     </>
