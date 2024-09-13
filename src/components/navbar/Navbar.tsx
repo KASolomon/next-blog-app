09427463b052b26df71700384d630b4d6929480e
KASolomon <kusiappiahsolomon10@gmail.com>
2024-09-13 15:04:31 +0000

    update nav links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,22 @@
 import React from "react";
+import Link from 'next/link'
 import Links from "./Links";
 import Menu from './Menu'
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center">
-      <div className="text-3xl font-bold ">Logo</div>
+      <div className="text-3xl font-bold ">
+        <Link href="/">Logo</Link>
+      </div>
       <div className="flex justify-end" style={{ minWidth: "60%" }}>
-        <div className={'hidden lg:block'}>
-        <Links/>
-
+        <div className={"hidden lg:block"}>
+          <div className={" flex gap-4"}>
+            <Links />
+          </div>
         </div>
-        <div className={'lg:hidden'} > 
-        <Menu/>
+        <div className={"lg:hidden"}>
 
+        <Menu />
         </div>
       </div>
     </div>
