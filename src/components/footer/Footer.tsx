80b2f469fdd651f5e27c74a1846dccffa58eebed
KASolomon <kusiappiahsolomon10@gmail.com>
2024-09-16 10:50:45 +0000

    make footer responsive

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <div className="flex justify-between text-gray-500">
+      <div className="flex flex-col gap-6  mt-6 text-center lg:flex-row lg:items-left justify-between text-gray-500">
 
       <p className="text-xl">lamadev</p>
       <p>Lama creative thoughts agency &copy; All rights reserved </p>
