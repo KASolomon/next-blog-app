b438f35108d9deb4e53ff3956838146bd4f5d811
KASolomon <kusiappiahsolomon10@gmail.com>
2024-09-16 10:08:43 +0000

    make about page responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Image from "next/image";
 const AboutPage = () => {
   return (
-    <div className="flex ">
+    <div className=" my-10 flex flex-col lg:flex-row lg:text-left text-center items-center">
       <div className=" flex gap-14 flex-col flex-1 ">
         <h3 className="text-2xl text-btn font-bold">About Agency</h3>
-        <h1 className="text-7xl font-bold">
+        <h1 className="text-5xl font-bold">
           We create digital ideas that are bigger, bolder, braver and better.
         </h1>
         <p>
@@ -13,7 +13,7 @@ const AboutPage = () => {
           believe in good ideas, flexibility, precision and quality. We give you
           the very best of service for your unique needs.
         </p>
-        <div className="flex justify-between">
+        <div className="flex flex-col lg:flex-row gap-4 justify-between">
           <div>
             <p className="text-4xl text-btn font-bold">70+</p>
             <p>Years of experience</p>
@@ -28,8 +28,8 @@ const AboutPage = () => {
           </div>
         </div>
       </div>
-      <div className="flex-1">
-        <div className="relative m-auto w-5/6 h-full ">
+      <div className="flex-1" style={{height : '50rem'}}>
+        <div className="relative m-auto w-5/6 h-5/6" >
           <Image src="/about.png" alt="" fill />
         </div>
       </div>
