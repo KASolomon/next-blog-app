6248bcb5b1b967e8837e17adad64aafb8a722daa
KASolomon <kusiappiahsolomon10@gmail.com>
2024-09-23 18:20:23 +0000

    fetch api data with axios

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import Image from "next/image";
-const SinglePost = () => {
+import appAxios from "@/config/appAxios"
+interface Props{
+  params :{[key : string] : string},
+  searchParams :{[key : string] : string},
+}
+
+const getPost = async (postId:number )=>{
+  try{
+    const post = await appAxios.get(`/posts/${postId}`)
+
+    return post.data
+  }catch(e){
+    console.log(e, 'Error while fetching post.')
+  }
+}
+
+const getUser = async (userId : number)=>{
+  try{
+    const userResponse = await appAxios.get(`/users/${userId}`)
+    return userResponse.data
+  }catch(e){
+console.log(e)
+  }
+}
+
+const SinglePost = async ({params, searchParams} : Props) => {
+  // console.log(params, searchParams)
+  const currentPost = await getPost(parseInt(params.slug))
+  const {username} = await  getUser(parseInt(currentPost.userId))
   return (
-    <div className="flex w-full h-full my-10">
+    <div className="flex w-full h-full my-10 justify-center lg:justify-start">
       <div className="relative w-1/3 min-h-screen overflow-hidden hidden lg:block">
         <Image
           src="https://images.unsplash.com/photo-1585079542156-2755d9c8a094?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -13,7 +41,7 @@ const SinglePost = () => {
       </div>
       <div className="flex flex-col w-2/3 gap-16 p-6 lg:ml-16">
         <div>
-          <h1 className="text-5xl font-bold">Title</h1>
+          <h1 className="text-5xl font-bold">{currentPost?.title}</h1>
         </div>
         <div className="flex  items-center gap-20">
           <div
@@ -37,7 +65,7 @@ const SinglePost = () => {
           </div>
           <div>
             <p className="text-gray-400 text-2xl">Author</p>
-            <p>Kwesi Benchi</p>
+            <p>{username}</p>
           </div>
           <div>
             <p className="text-gray-400 text-2xl">Published</p>
@@ -45,16 +73,7 @@ const SinglePost = () => {
           </div>
         </div>
         <div>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi sunt
-          sapiente commodi fugiat accusamus at sed sint earum rem officia,
-          repellendus culpa atque quidem! Dignissimos praesentium reiciendis
-          expedita molestiae sunt? Voluptates ipsam adipisci quas itaque quasi
-          sed fugit velit debitis voluptatibus laboriosam expedita earum labore
-          maiores molestiae repudiandae, nemo vero tenetur. Ipsum quam libero
-          atque ad. Quia blanditiis adipisci consequatur. Ullam, iure? Ducimus
-          quos iste quia fuga nulla iusto pariatur a consequatur repellat quis
-          tenetur optio sint dolorum iure vel dolor architecto, esse inventore
-          ut, vero magnam cum rerum! Magnam!
+         {currentPost?.body}
         </div>
       </div>
     </div>
