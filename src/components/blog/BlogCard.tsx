f7544a04c18672e665bbc71a58a114d276103ca1
KASolomon <kusiappiahsolomon10@gmail.com>
2024-09-23 18:21:46 +0000

    integrate mongodb data with mongoose

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -1,31 +1,27 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
-interface BlogItem{
-    title : string,
-    description : string,
-    image : string,
-    blog_link : string
-}
+import { Post } from './BlogGrid'
+
 
 interface Props{
-blogItem : BlogItem
+blogItem : Post
 }
 
 const BlogCard = ({blogItem}:Props) => {
   return (
     <div className=" px-2 flex">
       <div className="flex flex-col gap-6 " >
-        <div
+        {blogItem.img && <div
           className="relative rounded-lg overflow-hidden"
           style={{ width: "95%", height: "420px" }}
         >
-          <Image alt="" src={blogItem.image} className="object-cover " fill />
-        </div>
+          <Image alt="" src={blogItem.img} className="object-cover " fill />
+        </div>}
 
         <h3 className='text-3xl font-bold'>{blogItem.title}</h3>
         <p className='text-lg text-gray-500'>{blogItem.description}</p>
-        <Link href="/blog/post" className='underline'>READ MORE</Link>
+        <Link href={`/blog/${blogItem.slug}` }className='underline'>READ MORE</Link>
       </div>
       <div className="flex flex-col justify-center ">
         <p style={{ transform: "rotate(270deg)" }}>Date</p>
--- src/components/blog/BlogGrid.tsx
+++ src/components/blog/BlogGrid.tsx
@@ -1,81 +1,104 @@
 import React from "react";
 import BlogCard from "./BlogCard";
+import appAxios from "@/config/appAxios";
+import { ObjectId } from "mongoose";
+import { getPosts } from "@/data/data";
 
-const BlogGrid = () => {
-  const blogPosts = [
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1523289619259-44358ba02ba5?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1513366884929-f0b3bedfb653?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1596725858508-70543890c732?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1513366884929-f0b3bedfb653?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1523289619259-44358ba02ba5?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1523289619259-44358ba02ba5?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1596725858508-70543890c732?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1485988412941-77a35537dae4?q=80&w=1496&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-    {
-      title: "First Blog Post",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
-      image:
-        "https://images.unsplash.com/photo-1585079542156-2755d9c8a094?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      blog_link: "",
-    },
-  ];
+export interface Post{
+  author : ObjectId,
+  id : ObjectId,
+  title : string,
+  description : string,
+  img : string,
+  slug : string
+}
+// const getPosts = async ()=>{
+//   try{
+
+//     const posts = await appAxios.get('/posts')
+//     return posts.data
+//   }catch(e){
+//     console.log(e)
+//   }
+// }
+
+const BlogGrid = async () => {
+
+  const blogPosts : Post[] = await getPosts()
+  // const blogPosts = [
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1523289619259-44358ba02ba5?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1513366884929-f0b3bedfb653?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1596725858508-70543890c732?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1513366884929-f0b3bedfb653?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1523289619259-44358ba02ba5?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1523289619259-44358ba02ba5?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1596725858508-70543890c732?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1485988412941-77a35537dae4?q=80&w=1496&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  //   {
+  //     title: "First Blog Post",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi nulla cum quos praesentium quam quisquam, iure eum optio at, porro sequi nostrum iusto hic ex nobis. Ratione, nam distinctio!",
+  //     image:
+  //       "https://images.unsplash.com/photo-1585079542156-2755d9c8a094?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     blog_link: "",
+  //   },
+  // ];
   return (
     <div
 className=" py-6 my-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
@@ -88,7 +111,7 @@ className=" py-6 my-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
       }}
     >
       {blogPosts.map((post) => (
-        <BlogCard blogItem={post} />
+        <BlogCard blogItem={post} key={post.id.toString()} />
       ))}
     </div>
   );
