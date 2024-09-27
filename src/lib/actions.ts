46dc73fd92ec4c6e86fcf23a6da8cc2e5ab2a1bf
KASolomon <kusiappiahsolomon10@gmail.com>
2024-09-27 12:27:59 +0000

    define server actions - create authentication server actions - create registration server actions

---
+++ src/lib/actions.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import connectToMongo from "@/config/dbConn";
+import { auth, signIn, signOut } from "./auth";
+import bcrypt from 'bcryptjs'
+import { Users } from "@/config/models";
+export const handleGithubSignIn = async () => {
+  await signIn("github");
+  console.log(await auth())
+};
+export const handleGoogleSignIn = async () => {
+  await signIn("google");
+  console.log(await auth());
+};
+export const handleSignOut = async () => {
+  await signOut({redirectTo:'/'});
+
+};
+
+export const handleRegister = async (formData:FormData)=>{
+const {username, password, passwordRep, email} = Object.fromEntries(formData)
+console.log(password)
+if(password !== passwordRep){
+  throw new Error('Invalid data')
+}
+//hash password and store in database
+const hashSalt = bcrypt.genSaltSync(10)
+const passHash = bcrypt.hashSync(password.toString(),hashSalt)
+try {
+  connectToMongo()
+  await Users.create({
+    username, 
+    email, password : passHash
+  })
+  //give feedback about user success
+} catch (error) {
+  console.log(error)
+  throw new Error('Failed to create new user credentials')
+}
+
+}
