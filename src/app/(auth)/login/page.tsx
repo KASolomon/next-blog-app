2253e244bc27ffeac4d9f705af545c4441e0595a
KASolomon <kusiappiahsolomon10@gmail.com>
2024-10-11 00:45:52 +0000

    refactor : use login form components on page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,26 +1,39 @@
+import GithubLogin from "@/components/login/GithubLogin";
+import LoginForm from "@/components/login/LoginForm";
 import {
   handleGithubSignIn,
-  handleGoogleSignIn,
-  handleCredentialsSignIn,
+  handleGoogleSignIn
 } from "@/lib/actions";
-import React from "react";
-import {auth} from '@/lib/auth'
+import { auth } from "@/lib/auth";
+
+export const metadata ={
+  title: 'Login',
+  description : 'Log into you creative ideas account.'
+}
 
 const LoginPage = async () => {
-  console.log(await auth())
+console.log(await auth(), 'From login page')
+
   return (
-    <div>
-      <form action={handleGithubSignIn}>
-        <button>Github Login</button>
-      </form>
-      <form action={handleGoogleSignIn}>
-        <button>Google Login</button>
-      </form>
-      <form action={handleCredentialsSignIn}>
-        <input type="text" name="username" placeholder="Username" />
-        <input type="password" name="password" placeholder="Password" />
-        <button>Login</button>
-      </form>
+    <div
+      className="bg-bgSoft text-2xl  flex flex-col justify-center items-center text-black dark:text-white py-8 px-4"
+      style={{ width: "80vw", margin: "40px auto",  }}
+    >
+      <h3 className="font-bold text-4xl mb-24 text-center">Login</h3>
+      <div className="flex flex-col gap-14 " style={{ minWidth: "95%" }}>
+        <GithubLogin/>
+        <form action={handleGoogleSignIn} className="flex justify-center">
+          <button
+            className="bg-white rounded-md p-4 m-auto text-black "
+            style={{ width: "70%" }}
+          >
+            Google Login
+          </button>
+        </form>
+        <hr className="m-auto" style={{width:'60vw'}}/>
+        <h3 className="font-bold text-2xl text-center">Login with credentials</h3>
+        <LoginForm />
+      </div>
     </div>
   );
 };
