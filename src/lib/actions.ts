50758cf29f2d3c0a9374efd3ef032b46af6033b7
KASolomon <kusiappiahsolomon10@gmail.com>
2024-10-11 14:11:59 +0000

    create handleAddPost server action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,40 +3,31 @@
 import connectToMongo from "@/config/dbConn";
 import { auth, signIn, signOut } from "./auth";
 import bcrypt from "bcryptjs";
-import { Users } from "@/config/models";
-import { AuthFormState } from "@/components/register/RegisterForm";
+import { Posts, Users } from "@/config/models";
+import { AppFormState } from "@/components/register/RegisterForm";
 import { redirect } from "next/navigation";
-import { AuthError } from "next-auth";
 
-export const handleGithubSignIn = async (previousState: AuthFormState) => {
+export const handleGithubSignIn = async (previousState: AppFormState) => {
   try {
     await signIn("github", { redirectTo: "/" });
 
     return { success: true, message: "Logged in" };
   } catch (error: any) {
-    // console.log(error, 'Github action error');
-
     if (error.message == "NEXT_REDIRECT") throw error;
-
-    // if (error instanceof AuthError ){
-    //   return { error: true, message: error?.message };
-    // }
     return { error: true, message: "This github account has no user email." };
   }
 };
 export const handleGoogleSignIn = async () => {
   await signIn("google", { redirectTo: "/" });
-
 };
 
 export const handleCredentialsSignIn = async (
-  previousState: AuthFormState,
+  previousState: AppFormState,
   formData: FormData
 ) => {
   try {
     await signIn("credentials", formData);
 
-    
     return { success: true, message: "Login successful." };
   } catch (error: any) {
     if (error.message === "NEXT_REDIRECT")
@@ -50,7 +41,7 @@ export const handleSignOut = async () => {
 };
 
 export const handleRegister = async (
-  previousState: AuthFormState,
+  previousState: AppFormState,
   formData: FormData
 ) => {
   const { username, password, passwordRep, email } =
@@ -82,6 +73,41 @@ export const handleRegister = async (
     return {
       error: true,
       message: "Failed to create new user credentials. Please try again.",
+    };
+  }
+};
+
+interface Post {
+  title: string, 
+  description : string, 
+  img : string
+}
+export const handleAddPost = async (
+  previousState: AppFormState,
+  formData: FormData
+) => {
+  const {title, description,img } = Object.fromEntries(formData) as unknown as Post
+
+  //add the current user's ID as the value for the 'author' field when creating post. Add a slug as well
+
+
+  const activeSession = await auth()
+  const author = activeSession?.user._id
+  const slug = title.toLowerCase().split(" ").join('-')
+
+  try {
+    await Posts.create({
+      title, description, img, slug, author
+    })
+    return {
+      success: true,
+      message: "Blog updated.",
+    };
+  } catch (error) {
+    console.log(error)
+    return {
+      error: true,
+      message: "Failed to create post.",
     };
   }
 };
